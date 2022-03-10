6f95a2a2228b983e0e87ec75d527f2799576a984
Festus <sendze.philippe@gmail.com>
2022-03-10 12:53:14 +0100

    Initial commit, after the tutorial

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+export default function Footer () {
+
+    return (
+       <div className="header">
+            Copyright: Philippe Sendze
+       </div>  
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export default function Header () {
+
+    return (
+       <div className="header">
+            Header
+       </div>  
+    )
+}
